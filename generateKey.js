74bf07f6a9d994babd494a810fbed62149c8bb28
Bill <will.hdgli@gmail.com>
2022-11-30 17:30:49 -0600

    Add files via upload

---
+++ generateKey.js
@@ -0,0 +1,11 @@
+const secp = require("ethereum-cryptography/secp256k1");
+const {toHex} = require("ethereum-cryptography/utils");
+const {keccak256} = require("ethereum-cryptography/keccak");
+
+const privateKey = secp.utils.randomPrivateKey();
+const publicKey = secp.getPublicKey(privateKey);
+const address = keccak256(publicKey.slice(1,)).slice(-20,);
+
+console.log('PrivateKey: ', toHex(privateKey));
+console.log('PublicKey: ', toHex(publicKey));
+console.log("as Address: ", toHex(address));
